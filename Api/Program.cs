ec16f667457d929c325aadfdf1f45a84eb307446
Krzyśka Filip <45741043+Krzyska-Filip@users.noreply.github.com>
2026-05-10 18:52:48 +0200

    Remove swagger It doesn't work well with odata use case

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,4 +1,3 @@
-using Api.Filters;
 using Licenses.Database;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -11,63 +10,6 @@ using Microsoft.OpenApi;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.RegisterDatabase(builder.Configuration);
-
-/* Swagger */
-
-builder.Services.AddSwaggerGen(opt =>
-{
-    opt.CustomOperationIds(e =>
-        $"{e.HttpMethod}_{e.RelativePath?.Replace("/", "_").Replace("{", "").Replace("}", "").Replace("$", "").Trim('_')}");
-    opt.DocumentFilter<RemoveODataRoutes>();
-    opt.MapType<Delta<Group>>(() => new OpenApiSchema
-    {
-        Type = JsonSchemaType.Object,
-        Properties = new Dictionary<string, IOpenApiSchema>
-        {
-            ["name"] = new OpenApiSchema { Type = JsonSchemaType.String },
-            ["maintainerId"] = new OpenApiSchema { Type = JsonSchemaType.Integer | JsonSchemaType.Null }
-        }
-    });
-
-    opt.MapType<Delta<License>>(() => new OpenApiSchema
-    {
-        Type = JsonSchemaType.Object,
-        Properties = new Dictionary<string, IOpenApiSchema>
-        {
-            ["name"] = new OpenApiSchema { Type = JsonSchemaType.String },
-            ["type"] = new OpenApiSchema { Type = JsonSchemaType.String },
-            ["pricePerSeat"] = new OpenApiSchema { Type = JsonSchemaType.Number },
-            ["isProrated"] = new OpenApiSchema { Type = JsonSchemaType.Boolean },
-            ["validFrom"] = new OpenApiSchema { Type = JsonSchemaType.String | JsonSchemaType.Null, Format = "date" },
-            ["validTo"] = new OpenApiSchema { Type = JsonSchemaType.String | JsonSchemaType.Null, Format = "date" },
-            ["seats"] = new OpenApiSchema { Type = JsonSchemaType.Integer },
-            ["groupId"] = new OpenApiSchema { Type = JsonSchemaType.Integer },
-            ["previousId"] = new OpenApiSchema { Type = JsonSchemaType.Integer | JsonSchemaType.Null }
-        }
-    });
-
-    opt.MapType<Delta<Seat>>(() => new OpenApiSchema
-    {
-        Type = JsonSchemaType.Object,
-        Properties = new Dictionary<string, IOpenApiSchema>
-        {
-            ["licenseId"] = new OpenApiSchema { Type = JsonSchemaType.Integer },
-            ["assignedToId"] = new OpenApiSchema { Type = JsonSchemaType.Integer | JsonSchemaType.Null },
-            ["proratedPurchase"] = new OpenApiSchema { Type = JsonSchemaType.Boolean },
-            ["validFrom"] = new OpenApiSchema { Type = JsonSchemaType.String, Format = "date" }
-        }
-    });
-
-    opt.MapType<Delta<User>>(() => new OpenApiSchema
-    {
-        Type = JsonSchemaType.Object,
-        Properties = new Dictionary<string, IOpenApiSchema>
-        {
-            ["username"] = new OpenApiSchema { Type = JsonSchemaType.String },
-            ["email"] = new OpenApiSchema { Type = JsonSchemaType.String }
-        }
-    });
-});
 
 /* ODATA */
 
@@ -106,8 +48,6 @@ app.Services.MigrateDatabase();
 if (app.Environment.IsDevelopment())
 {
     app.Services.SeedDatabase();
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
 
 app.UseCors("CorsPolicy");
